82b5e249130c434d6df15df47d1d226b3c5e6f5e
Chijindu Nwokeohuru <chiji4collage@gmail.com>
2023-09-27 13:46:14 +0100

    Update Registeration functions

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react"
-import {toast} from 'react-toastify'
-import {FaUser} from "react-icons/fa"
-import {useSelector, useDispatch} from 'react-redux'
-import {register} from '../features/auth/authSlice'
+import { useState, useEffect} from "react"
+import { useNavigate } from "react-router-dom"
+import { toast } from 'react-toastify'
+import {  FaUser } from "react-icons/fa"
+import { useSelector, useDispatch } from 'react-redux'
+import { register, reset } from '../features/auth/authSlice'
+import Spinner from "../components/Spinner"
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -15,8 +17,24 @@ function Register() {
   const {name, email, password, password2} = formData
 
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  const {user, isLoading, isSuccess, message} = useSelector(state => state.auth)
+  const {user, isLoading, isError, isSuccess, message} = useSelector(
+      state => state.auth
+    )
+
+  useEffect(() => {
+    if(isError) {
+      toast.error(message)
+    }
+
+    // Redirect when loggined in
+    if(isSuccess || user) {
+      navigate('/')
+    } 
+
+    dispatch(reset())
+  }, [isError, isSuccess, user, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -41,6 +59,9 @@ function Register() {
     }
   }
 
+  if(isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
