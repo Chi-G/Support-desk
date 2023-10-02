e9365ec91b54d0a16770b102d9e76fc21f0a932b
Chijindu Nwokeohuru <chiji4collage@gmail.com>
2023-10-02 14:34:44 +0100

    Update on userController.js

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -74,7 +74,7 @@ const loginUser = asyncHandler(async(req, res) => {
 // @access  Private
 const getMe = asyncHandler(async(req, res) => {
   const user = {
-    id: req.user._id,
+    id: req.user._id, 
     emai: req.user.email,
     name: req.user.name
   }
